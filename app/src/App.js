291d0095362616ff5eda7110b584474279d43fa4
sachinxsharma <sachinsharma72003@gmail.com>
2026-01-29 17:20:32 +0530

    Converted app from submodule to normal folder

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign:"center", marginTop:"50px", color:"white", backgroundColor:"black"}}>
+      <h1>Version 1 🚀 Deploy Forge Working</h1>
+      <p>Auto Deployment System Built From Scratch</p>
+            <p style={{textAlign:"center"}}>@sachinxsharma</p>
+    </div>
+  );
+}
+
+
+export default App;
